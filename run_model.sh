dc68621c2bda053f0aa89841a22fdca3520c4dbb
Chris Bruner <cwbruner@gmail.com>
2026-04-30 18:59:35 -0400

    latest run

--- run_model.sh
+++ run_model.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Run the quaternary model built by teacher_pipeline.py
-# Usage: run_model.sh [prompt text]
-PROMPT="${*:-<|Q|>}"
+# Run the quaternary model
+PROMPT="${1:-<|Q|>}"
 ./llama.cpp/build/bin/llama-simple -m quaternary_trained.gguf --no-warmup -ngl 0 -n 50 -p "$PROMPT" 2>/dev/null
